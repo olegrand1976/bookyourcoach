1240eb3304a86c4857ff48e7838787ab3d3c2bf3
Olivier LEGRAND <o.legrand@ll-it-sc.be>
2025-09-10 09:04:07 +0200

    update github action

--- frontend/.nuxt/types/nitro-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-export type MiddlewareKey = "admin" | "auth-admin" | "auth" | "role-control" | "student"
-declare module 'nitropack' {
-  interface NitroRouteConfig {
-    appMiddleware?: MiddlewareKey | MiddlewareKey[] | Record<MiddlewareKey, boolean>
-  }
-}
