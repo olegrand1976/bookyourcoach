eee18e1b82e2a8eee5c23c166d568aa28691ceae
Olivier LEGRAND <o.legrand@ll-it-sc.be>
2025-09-19 18:34:31 +0200

    clean project frontend

--- frontend/.nuxt/dist/server/_nuxt/colors-demo-styles.BfAcGz-6.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './colors-demo-styles-1.mjs-CNkDGTWn.js';
-export default [style_0]
--- frontend/.nuxt/dist/server/_nuxt/lessons-styles.CIFXN7DA.mjs
+++
@@ -1,3 +0,0 @@
-import style_0 from './lessons-styles-1.mjs-Bhze-H_0.js';
-import style_1 from './lessons-styles-1.mjs-Bhze-H_0.js';
-export default [style_0, style_1]
